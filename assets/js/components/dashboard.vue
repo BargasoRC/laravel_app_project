421d0b3f5a3c197960f5d681221c3d402c25d2be
unknown <2ndyrGroupB@PNC1-26.pn.local>
2020-01-22 11:58:30 +0800

    continue

---
+++ assets/js/components/dashboard.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-card
+    class="mx-auto"
+    max-width="100%"
+    outlined
+  >
+    <apexchart width="100%" type="bar" :options="options" :series="series"></apexchart>
+  </v-card>
+</template>
+<script>
+    import Vue from 'vue'
+    import VueApexCharts from 'vue-apexcharts'
+    Vue.component('apexchart', VueApexCharts)
+    export default {
+        data: () => {
+            return {
+            options: {
+                chart: {
+                id: 'vuechart-example'
+                },
+                xaxis: {
+                categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998]
+                }
+            },
+            series: [{
+                name: 'series-1',
+                data: [30, 40, 45, 50, 49, 60, 70, 91]
+            }]
+            }
+        }
+    }
+</script>
---
+++ assets/js/views/admin.vue
@@ -0,0 +1,97 @@
+<template>
+  <v-app id="sandbox">
+    <v-navigation-drawer
+      v-model="primaryDrawer.model"
+      :clipped="primaryDrawer.clipped"
+      permanent
+      :mini-variant="primaryDrawer.mini"
+      app
+      overflow
+    >
+      <center><v-img class="avatar" src="https://randomuser.me/api/portraits/men/85.jpg"></v-img></center>
+      <v-divider></v-divider>
+
+      <v-list
+        dense
+        nav
+      >
+        <v-list-item
+          v-for="item in items"
+          :key="item.title"
+          link
+        >
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar
+      :clipped-left="primaryDrawer.clipped"
+      app
+    > 
+    <v-icon @click="is_permanent">
+      fas fa-bars
+    </v-icon>
+      <v-toolbar-title>&nbsp;&nbsp;Vuetify</v-toolbar-title>
+    </v-app-bar>
+
+    <v-content>
+      <v-container fluid>
+        <v-row
+          align="center"
+          justify="center"
+        >
+          <v-col cols="10">
+            <dashboard></dashboard>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-content>
+  </v-app>
+</template>
+<style scoped>
+  .avatar{
+    margin-top:50px;
+    margin-bottom:50px;
+    height:100px;
+    width:95px;
+    border-radius:50px;
+  }
+</style>
+
+<script>
+
+  import dashboard from "../components/dashboard.vue";
+
+  export default {
+    components:{
+      dashboard,
+    },
+    data: () => ({
+      items: [
+          { title: 'Dashboard', icon: 'fas fa-tachometer-alt' },
+          { title: 'Bookings', icon: 'fas fa-book'},
+          { title: 'Buses', icon: 'fas fa-bus-alt'},
+          { title: 'Schedules', icon: 'far fa-calendar-alt' },
+          { title: 'My Account', icon: 'far fa-user-circle' },
+        ],
+      primaryDrawer: {
+        model: null,
+        clipped: true,
+        mini:true
+      },
+     
+    }),
+    methods:{
+      is_permanent(){
+        this.primaryDrawer.mini = !this.primaryDrawer.mini;
+      }
+    }
+  }
+</script>
